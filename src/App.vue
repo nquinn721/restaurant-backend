88977d3e96ac9e2d5371c89cb4f49814d07c79ff
Nathan <nquinn@medonesystems.com>
2020-08-29 20:18:15 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+#app
+  #nav
+    router-link( to="/") Home|
+    router-link( to="/about") About
+  router-view
 </template>
-
+<script>
+import { MainStore } from "./store/Main.mobx";
+export default {
+  data() {
+    return {
+      MainStore,
+    };
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/store/models/Item.model.ts
@@ -0,0 +1,4 @@
+import { Model } from "mobx-store-model";
+export class Item extends Model {
+  route: string = "item";
+}
