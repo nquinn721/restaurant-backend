64ff8538370f93ffcee0137251912220965ec053
Nathan <nquinn@medonesystems.com>
2020-09-01 17:41:33 -0400

    wef

--- src/store/Main.mobx.ts
+++ src/store/Main.mobx.ts
@@ -1,5 +1,7 @@
 import { Store } from "mobx-store-model";
 import { Order } from "./models/Order.model";
+import { Service } from "mobx-store-model";
+Service.setBaseUrl("https://restaurant-server-288018.ue.r.appspot.com/");
 class MainMobx {
   orders = new Store(Order);
 
--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,7 +1,12 @@
 <template lang="pug">
 div
   h1 Orders
-  div(v-for="item in MainStore.orders.objects" :key="item.id") {{item.name}}
+  div(v-for="order in MainStore.orders.objects" :key="order.id") 
+    div(v-for="item in order.items" :key="item.id") 
+      h2 Order - {{item.id}}
+      | {{item.item.name}}
+      div(v-for="mod in item.mods" :key="mod.id") Mod: {{mod.modification.name}}
+      div(v-for="side in item.sides" :key="side.id") Side: {{side.side.name}}
 
 </template>
 
