449742bd51216be8e1d02e2c35a854f8a4739a38
Nathan <nquinn@medonesystems.com>
2020-09-03 22:52:29 -0400

    wef

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
 v-container
   h1 Orders
-  v-card.pa-5.justify-space-between.d-flex(v-for="order in MainStore.orders.objects" :key="order.id") 
+  v-card.pa-5.ma-5.justify-space-between.d-flex(v-for="order in MainStore.orders.objects" :key="order.id") 
     div(v-for="item in order.items" :key="item.id") 
-      h2 Order - {{item.id}}
+    
       | {{item.item.name}}
       div(v-for="mod in item.mods" :key="mod.id") Mod: {{mod.modification.name}}
       div(v-for="side in item.sides" :key="side.id") Side: {{side.side.name}}
     div
+      div.mb-5(style="color: #999;text-align:right") # {{order.id}}
       v-btn(color="primary") Complete
 
 </template>
